6cf7931fd3c1acbb98d1d488ec38414c4339509c
sawergon <sasha.sesyukalov@gmail.com>
2025-03-11 11:25:42 +0300

    форматирование кода

--- src/query/last_year_statistic.sql
+++ src/query/last_year_statistic.sql
@@ -2,12 +2,9 @@ select c.name                            as category_name,
        monthname(p.date)                 as month_name,
        sum(pi.amount * pi.inorder_price) as total_sales
 from purchase p
-         join
-     purchase_items pi on p.id = pi.purchase_id
-         join
-     product pr on pi.product_id = pr.id
-         join
-     category c on pr.category_code = c.code
+         join purchase_items pi on p.id = pi.purchase_id
+         join product pr on pi.product_id = pr.id
+         join category c on pr.category_code = c.code
 where year(p.date) = year(curdate()) - 1
 group by c.name, year(p.date), month_name
 order by month_name;
